fab741c897b518298568369b2aa6f9d9efa8628a
donald530-a <128647286+donald530-a@users.noreply.github.com>
2023-03-31 22:35:14 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+function showEnglish() {
+    document.getElementById("engVer").style.display = "";
+    document.getElementById("chiVer").style.display = "none";
+    document.getElementById("toptitle").innerHTML = "Fitness Walking Tracks";
+    document.getElementById("engInput").style.display = "";
+    document.getElementById("chiInput").style.display = "none";
+
+}
+
+function showChinese() {
+    document.getElementById("engVer").style.display = "none";
+    document.getElementById("chiVer").style.display = "";
+    document.getElementById("toptitle").innerHTML = "健步行徑";
+    document.getElementById("engInput").style.display = "none";
+    document.getElementById("chiInput").style.display = "";
+}
+
+function searchEngTable() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("engInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tb1");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+
+  function searchChiTable() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("chiInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tb2");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+
+
